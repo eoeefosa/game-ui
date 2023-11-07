6f7b8399e8375883498245ed5e6327834a4527a2
eoeefosa <eoeefosa@gmail.com>
2023-11-07 01:52:51 +0100

    added crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,60 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
+import 'package:simplegame/firebase_options.dart';
 import 'package:simplegame/routes.dart';
 import 'package:simplegame/src/app_lifecycle/app_lifecycle.dart';
+import 'package:simplegame/src/carashlytics/crashlytics.dart';
 import 'package:simplegame/src/settings/persistence/local_storage_settings_persistences.dart';
 import 'package:simplegame/src/settings/persistence/settings_persistence.dart';
 import 'package:simplegame/src/settings/settings.dart';
 import 'package:simplegame/src/style/palette.dart';
 import 'package:simplegame/src/style/snack_bar.dart';
 
-// Future<void> main() async {
-//   FirebaseCrashlytics? crashlytics;
-//   if(!kIsWeb&& (Platform.isIOS||Platform.isAndroid)){
-//     try{
-//       WidgetsFlutterBinding.ensureInitialized();
-//       await Fire
-//     }
-//   }
-// }
+Future<void> main() async {
+  FirebaseCrashlytics? crashlytics;
+  if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      crashlytics = FirebaseCrashlytics.instance;
+    } catch (e) {
+      debugPrint("Firebase Couldn't be initialized:$e");
+    }
+  }
 
-void main() {
+  await guardWithCrashlytics(
+    guardedMain,
+    crashlytics: crashlytics,
+  );
+}
+
+void guardedMain() {
+  if (kReleaseMode) {
+    Logger.root.level = Level.WARNING;
+  }
+  Logger.root.onRecord.listen((record) {
+    debugPrint('${record.level.name}:${record.time}:'
+        '${record.loggerName}'
+        '${record.message}');
+  });
   WidgetsFlutterBinding.ensureInitialized();
+  _log.info('Going  full screen');
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+  );
   runApp(MyApp());
 }
+
+Logger _log = Logger('main.dart');
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
